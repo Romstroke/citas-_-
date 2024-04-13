33559c4965623ea272c0f1cf2fbe252d8ffad293
Romina <rominchuela@gmail.com>
2024-04-13 02:24:47 -0400

    partition v2.0

--- index.js
+++ index.js
@@ -22,27 +22,56 @@ app.use(express.json());
 
 // nombre: Vicenta - Apellido: Marin - ID: 37adc3 - Timestamp: November 4th 2021, 7:32:35 pm
 
-app.get('/randomuser', async (req, res) => {
+app.get('/11registros', async (req, res) => {
   try {
     //leyendo una api con Axios
     const response = await axios.get('https://randomuser.me/api/?results=11');
     const objectData = response.data;
     const userData = objectData.results;
     // console.log('Fecha registro: ', userData[0].registered.date);
-    
-    userData.forEach(user => {
-      const segundos = Math.floor(Math.random() * 60) +1;
-      const fechaRegistro = moment().add(segundos,'seconds').format('MMMM Do YYYY, h:mm:ss a');
-      const usuarioCreado = `Nombre: ${user.name.first} - Apellido: ${user.name.last} - ID: ${uuidv4().slice(0, 6)} - Timestamp: ${fechaRegistro}\n`;
+
+    userData.forEach((user, index) => {
+      const segundos = Math.floor(Math.random() * 60) + 1;
+      const fechaRegistro = moment().add(segundos, 'seconds').format('MMMM Do YYYY, h:mm:ss a');
+      // const usuarioCreado = `${index + 1}. Nombre: ${user.name.first} - Apellido: ${user.name.last} - ID: ${uuidv4().slice(0, 6)} - Timestamp: ${fechaRegistro}\n`;
+      const usuarioCreado = {
+        id: uuidv4().slice(0, 6),
+        nombre: user.name.first,
+        apellido: user.name.last,
+        genero: user.gender,
+        timestamp: fechaRegistro
+      };
       usuariosRegistrados.push(usuarioCreado);
+      console.log(chalk.blue.bgWhite.bold(` ${index + 1}. Nombre: ${user.name.first} - Apellido: ${user.name.last} - ID: ${uuidv4().slice(0, 6)} - Timestamp: ${fechaRegistro} `));
     });
-    
-    console.log(chalk.blue.bgWhite.bold(usuariosRegistrados.join('')));
-    const usuariosPorGenero = _.partition(userData, (usuario) => usuario.gender == 'male');
-    console.log(usuariosPorGenero);
-    
-    // conversion a Json, devuelve un objeto con un usuario
-    res.json(userData);
+
+    //usuarios por genero
+
+    const usuariosPorGenero = _.partition(usuariosRegistrados, (usuario) => usuario.genero === 'male');
+    const mujeres = [];
+    const hombres = [];
+
+    usuariosPorGenero[1].forEach((mujer, index) => {
+      const individua = `${index + 1}. Nombre: ${mujer.nombre} - Apellido: ${mujer.apellido} - ID: ${mujer.id} - Timestamp: ${mujer.timestamp}`;
+      mujeres.push(individua);
+    });
+
+    usuariosPorGenero[0].forEach((hombre, index) => {
+      const individuo = `${index + 1}. Nombre: ${hombre.nombre} - Apellido: ${hombre.apellido} - ID: ${hombre.id} - Timestamp: ${hombre.timestamp}`;
+      hombres.push(individuo);
+    });
+
+    const porGenero = `
+     <p>Mujeres:</p>
+     ${mujeres.join('</br>')}
+     
+     <p>Hombres:</p>
+      ${hombres.join('</br>')}
+    `;
+
+    res.send(porGenero);
+
+
   } catch (error) {
     console.error('Error fetching random user data:', error);
     res.status(500).json({ error: 'Error fetching random user data' });
